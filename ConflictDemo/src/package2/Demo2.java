691263aabbeeeef817fefe672fa9196d82cb6bda
Shobha8 <shobhaprakash2013@gmail.com>
2023-04-03 17:36:20 -0400

    Demo2 commit

---
+++ ConflictDemo/src/package2/Demo2.java
@@ -0,0 +1,15 @@
+package package2;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome");
+		System.out.println("welcome");
+		System.out.println("welcome");
+		System.out.println("welcome");
+		System.out.println("welcome");
+System.out.println("Hello");
+System.out.println("HI");
+	}
+
+}
